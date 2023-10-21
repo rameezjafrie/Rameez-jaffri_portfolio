e9cdcd1b783ee614bc0b2f94021709ff299a0ac3
rameezjafrie <hurjaffri666666@gmail.com>
2023-10-22 01:16:28 +0500

    About updated

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name is Rahul Patil, and I'm a full-stack software engineer who's always chasing the latest tech trends. I'm fluent in ReactJS, MySQL, ASP.NET, and Machine Learning - so basically, I speak both human and machine languages.",
+    description1: "My name is Rameez jaffri, and I'm a full-stack software engineer who's always chasing the latest tech trends. I'm fluent in ReactJS, MongoDB, Nodejs, and ExpressJs - so basically, I speak both human and machine languages.",
     description2: "And when it comes to prompt engineering, I've had some great teachers - ChatGPT and Midjourney have taught me everything I know about making machines sound like they're almost as smart as me.",
     image: 1
 }
