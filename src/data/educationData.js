fa7ebbe34b3db31410ad79a4cd92b4a7eb6f2571
rameezjafrie <hurjaffri666666@gmail.com>
2023-10-22 01:30:24 +0500

    Education Updated

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,23 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'University of Mumbai',
-        course: 'Bachelor of Engineering in Information Technology',
-        startYear: '2018',
-        endYear: '2022'
+        institution: 'University of Karachi',
+        course: 'Bachelor of Commerce',
+        startYear: '2009',
+        endYear: '2011'
     },
     {
         id: 2,
-        institution: 'Smt. C.H.M. College of Arts, Commerce & Science',
+        institution: 'Govt Degress Science and Commerce Collage',
         course: 'Class XII Science',
-        startYear: '2016',
-        endYear: '2018'
+        startYear: '2007',
+        endYear: '2008'
     },
     {
         id: 3,
-        institution: 'P.M.M. Rotary School & Jr. College',
+        institution: 'Al Sahar Academy',
         course: 'Class X SSC',
-        startYear: '2006',
-        endYear: '2016'
+        startYear: '2005',
+        endYear: '2006'
     },
 ]
