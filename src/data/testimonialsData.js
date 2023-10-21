001013ca1a9bb5c29cd0f6f327bf7c1898fad431
rameezjafrie <hurjaffri666666@gmail.com>
2023-10-22 03:13:06 +0500

    Qoutes of imam ali Testimonals

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -7,23 +7,23 @@ import test2 from '../assets/png/Test4.png'
 export const testimonialsData = [
     {
         id: 1,
-        name: 'Anup Kunte',
-        title: 'Professor, Artificial Intelligence',
-        text: "My opinion of Rahul is of an incredibly disciplined and punctual student who can manage time efficiently to deliver the tasks assigned within the stipulated deadline.",
+        name: 'وَ قَالَ ( عليه‏السلام(: الْبُخْلُ عَارٌ وَ الْجُبْنُ مَنْقَصَةٌ وَ الْفَقْرُ يُخْرِسُ الْفَطِنَ عَنْ حُجَّتِهِ وَ الْمُقِلُّ غَرِيبٌ فِي بَلْدَتِهِ',
+        title: "Amir al-mu'minin, peace be upon him, said",
+        text: "Miserliness is shame; cowardice is a defect; poverty disables an intelligent man from arguing his case; and a destitute person is a stranger in his home town.",
         image: test1
     },
     {
         id: 2,
-        name: 'Vijay Kharche',
-        title: 'Professor, Cryptography & Network Security',
-        text: "In Rahul, I saw an impressive combination of an analytical mind-set and the ability to do smart work.",
+        name: ' قَال(عليه السلام): كُنْ فِي الْفِتْنَةِ كَابْنِ اللَّبُونِ ، لاَ ظَهْرٌ فَيُرْكَبَ، وَلاَ ضَرْعٌ فَيُحْلَبَ.',
+        title: "Amir al-mu'minin, peace be upon him, said",
+        text: "During civil disturbance be like an adolescent camel 1 who has neither a back strong enough for riding nor udders for milking.",
         image: test1
     },
     {
         id: 3,
-        name: 'Gaurav Takrani',
-        title: 'Director, GTLB Infotech Pvt. Ltd.',
-        text: "He has done everything exquisitely. Best project in this batch to date!",
+        name: 'وَ قَالَ ( عليه‏السلام): الْعَجْزُ آفَةٌ وَ الصَّبْرُ شَجَاعَةٌ وَ الزُّهْدُ ثَرْوَةٌ وَ الْوَرَعُ جُنَّةٌ وَ نِعْمَ الْقَرِينُ الرِّضَى',
+        title: "Amir al-mu'minin, peace be upon him, said",
+        text: "Incapability is a catastrophe; endurance is bravery; abstinence is riches; self-restraint is a shield (against sin); and the best companion is submission (to Allah's will).",
         image: test2
     },
     
